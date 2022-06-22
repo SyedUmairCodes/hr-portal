2331f89d549207da46b50643886ee8188d634c49
Syed Umair Ali <syedumairali.617@gmail.com>
2022-06-22 18:21:56 +0500

    Created main routes using react-router

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,18 +1,20 @@
-import logo from '../assets/images/logo.svg';
-import mainImage from '../assets/images/main.svg'
+import Logo from "../components/Logo";
+import mainImage from '../assets/images/main-alternative.svg'
 import Wrapper from '../assets/wrappers/LandingPage'
+import {Link} from "react-router-dom";
+
 function Landing() {
     return (
         <Wrapper>
                 <nav>
-                    <img src={logo} alt="logo" className="logo"/>
+                <Logo/>
                 </nav>
                 <div className="container page">
                     <div className="info">
                         <h1>Jobs <span>Tracking</span> Application</h1>
                         <p>Keep track of the number of jobs and applications easily. A great way to get new jobs
                             and applications from great candidates</p>
-                        <button className="btn btn-hero">Login/Register</button>
+                        <Link to="/register" className="btn btn-hero">Login/Register</Link>
                     </div>
                     <img src={mainImage} alt="landing" className="img main-img"/>
                 </div>
